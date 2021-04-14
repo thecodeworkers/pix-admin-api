c3ef930de7f4737c6f3a2fd66bb632365f8b4ecd
Victor Caceres <caceres.vc95@gmail.com>
2021-04-14 16:37:56 -0400

    Add extra options in console programs

---
+++ app/console/__init__.py
@@ -0,0 +1,3 @@
+from .key_generation import *
+from .colors import *
+from .default_ask import *
--- setup.py
+++ setup.py
@@ -1,53 +1,20 @@
-from dotenv import dotenv_values
-from app.config.keys import create_application_token
-import secrets
+from app.console import *
 
 if __name__ == '__main__':
-    ENDC = '\033[0m'
-    OKGREEN = '\033[92m'
-    FAIL = '\033[91m'
-
-    def _generate_keys():
-        key = secrets.token_urlsafe(32)
-        secret_key = secrets.token_urlsafe(40)
-        app_token = create_application_token(key, secret_key)
-
-        print('')
-        print(f' APP_KEY: {OKGREEN}{key}{ENDC}')
-        print(f' APP_SECRET: {OKGREEN}{secret_key}{ENDC}')
-        print(f' APP_TOKEN: {OKGREEN}{app_token}{ENDC}')
-        print('\n\033[94m APP_KEY and APP_SECRET must go in the .env')
-        print(f' APP_TOKEN must be in request header as X-API-Key{ENDC}')
-        print('')
-
-    def _input_asked(prompt=''):
-        while True:
-            try:
-                n = int(input(prompt))
-                if n < 0 or n > 1: raise ValueError
-                break
-
-            except ValueError:
-                print(f'\n {FAIL}Invalid choice! Enter 0 or 1.{ENDC}')
-                prompt = '\n Do you want to generate the keys? '
-
-        return n
-
     try:
-        print(f'\033[96m\n You are about to create the keys for the application, this program will generate three keys, two of them must be placed in the app .env and another must be used to make requests to the api \n{ENDC}')
-        print(f""" {OKGREEN}1) Generate Keys{ENDC} \n 0) Exit \n""")
-        choice = _input_asked(' What\'s is your choice: ')
+        print(f""" {OKGREEN}1) Key Generation{ENDC} \n {OKGREEN}2) Migrate Database{ENDC} \n 0) Exit \n""")
+        option_text = 'Select a option:'
+        choice = input_asked(f' {option_text} ', option_text, 2)
 
         if not choice:
             print('')
             quit()
 
-        if choice:
-            _generate_keys()
+        if choice == 1:
+            key_generation_program()
 
-    except Exception:
-        print(f'\n {FAIL}Error occurred.{ENDC}')
-        print('')
+        if choice == 2:
+            pass
 
     except KeyboardInterrupt:
         print('\n')
