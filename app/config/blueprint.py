db0bec96c12ad94fa431fbdab67159dc90377368
Victor Caceres <caceres.vc95@gmail.com>
2021-04-16 00:04:24 -0400

    Register blueprints

--- app/config/blueprint.py
+++ app/config/blueprint.py
@@ -1,6 +1,10 @@
-from ..controllers.users import auth, user, role
+from ..controllers.users import auth, user, role, permission
+from ..controllers.resources import currency, language
 
 def init_blueprints(app):
     app.register_blueprint(auth.bp)
     app.register_blueprint(user.bp)
     app.register_blueprint(role.bp)
+    app.register_blueprint(permission.bp)
+    app.register_blueprint(currency.bp)
+    app.register_blueprint(language.bp)
--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -3,5 +3,8 @@ from .verify_token import verify_app_token
 middlewares = {
     'auth': [verify_app_token],
     'user': [verify_app_token],
-    'role': [verify_app_token]
+    'role': [verify_app_token],
+    'permission': [verify_app_token],
+    'currency': [verify_app_token],
+    'language': [verify_app_token]
 }
